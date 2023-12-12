79d0b52a98fda4e1f966205bd0e1cd1229a48df6
Jovan Brzakovic <jova3105@gmail.com>
2023-12-12 15:11:33 +0100

    MongoDb connection

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
     console.log(`Server running on port: ${port}`)
 });
+
+mongoose.connect(uri
+//     ,{
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+// } Deprecated
+).then(() => console.log("MongoDB connection established")).catch((error) => console.log("MongoDB connection failed: ", error.message))
