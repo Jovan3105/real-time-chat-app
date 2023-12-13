6f8226005f3c38b29241b0a4d5926714f08c329f
Jovan Brzakovic <jova3105@gmail.com>
2023-12-13 15:13:22 +0100

    Setting routes - react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+import Chat from "./pages/Chat"
+import Register from "./pages/Register"
+import Login from "./pages/Login"
+
 function App() {
 
   return (
-    <>Chat app</>
+    <>
+      <Routes>
+        <Route path="/" element={<Chat></Chat>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
+      </Routes>
+    </>
   )
 }
 
