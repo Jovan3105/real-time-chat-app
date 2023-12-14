7f7261b0d59b3ee286c073f447f0a600266511cc
Jovan Brzakovic <jova3105@gmail.com>
2023-12-14 15:46:16 +0100

    Setting up bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import { Routes, Route, Navigate } from "react-router-dom"
-import Chat from "./pages/Chat"
-import Register from "./pages/Register"
-import Login from "./pages/Login"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 function App() {
 
   return (
-    <>
+    <Container className="text-secondary">
       <Routes>
         <Route path="/" element={<Chat></Chat>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
       </Routes>
-    </>
-  )
+    </Container>
+  );
 }
 
-export default App
+export default App;
