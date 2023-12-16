e12d9f449b7e642637e82dccaea910439576b395
Jovan Brzakovic <jova3105@gmail.com>
2023-12-16 14:21:34 +0100

    Register user - frontend, backend

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,27 +1,51 @@
 import { createContext, useCallback, useState } from "react";
+import { baseUrl, postRequest } from "../utils/services";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    const [registerError, setRegisterError] = useState(null);
+    const [isRegisterLoading, setIsRegisterLoading] = useState(false);
     const [registerInfo, setRegisterInfo] = useState({
         name: "",
         email: "",
         password: ""
     });
 
-    console.log("registerInfo", registerInfo);
+    //console.log("registerInfo", registerInfo);
 
     const updateRegisterInfo = useCallback((info) => {
         setRegisterInfo(info)
     }, []);
 
+    const registerUser = useCallback(async (e) => {
+
+        e.preventDefault(); // prevent reloading the page when submiting a form
+        setIsRegisterLoading(true);
+        setRegisterError(null); // reset the presence of an error
+
+        const response = await postRequest(`${baseUrl}/users/register`, JSON.stringify(registerInfo))
+
+        setIsRegisterLoading(false);
+
+        if (response.error) {
+            return setRegisterError(response);
+        }
+        // if there is no error then set user
+        localStorage.setItem("User", JSON.stringify(response));
+        setUser(response);
+    
+    }, [registerInfo]);
 
     return <AuthContext.Provider value={{
         user,
         registerInfo,
-        updateRegisterInfo
+        updateRegisterInfo,
+        registerUser,
+        registerError,
+        isRegisterLoading
     }}>
         {children}
     </AuthContext.Provider>
